29d72616717e7026f14e9fc6d168c25f7cd277b4
bilal messey <messey.bilal@gmail.com>
2022-03-18 18:20:04 +0100

    fifteenth update: updating of download.js page

--- downloadacceleratorsite/downloadaccelerator/static/downloadaccelerator/download.js
+++ downloadacceleratorsite/downloadaccelerator/static/downloadaccelerator/download.js
@@ -17,16 +17,31 @@ $(function() {
     
     $('#btn-createAccount').click(
       function () {
+        var err = false;
         if(($('#name').val().length === 0) | (!(/^[a-zA-Z][a-zA-Z0-9_-]*$/.test($('#name').val())))){
           $('#name').css('border-color', 'red');
+          $('#name').tooltip({placement: 'right'}).tooltip('show');
+          err = true;
         }
-        else {
-          $('#name').css('border-color', 'blue');
+        if(($('#email').val().length === 0) |(!(/^[a-zA-Z](?:[a-zA-Z0-9_.-])*@[a-zA-Z]+\.[a-z]{2,5}/.test($('#email').val())))){
+          $('#email').addClass('border-danger');
+          $('#email').tooltip({placement: 'right'}).tooltip('show');
+          err = true;
+        }
+        else{
+          if ($('#email').hasClass('border-danger')) {
+            $('#email').removeClass('border-danger');
+            $('#email').tooltip('disable');
+          }
         }
         if($('input[placeholder = "Create password"]').val() != $('input[placeholder = "Repeat password"]').val()){
           $('input[placeholder = "Create password"]').css('border-color', 'red');
           $('input[placeholder = "Repeat password"]').css('border-color', 'red');
+          $('input[placeholder = "Repeat password"]').attr('title', 'This password is not equal to the previous ! Please, fix this').tooltip({placement: 'right'}).tooltip('show');
+          err = true;
         }
+        if(err)
+          return false;
       }
     );
     
