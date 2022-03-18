7dfb9999d10999c54c1c1db3bfe4479b4c81e714
bilal messey <messey.bilal@gmail.com>
2022-03-18 09:40:31 +0100

    fourteenth update: updating of download.js page

--- downloadacceleratorsite/downloadaccelerator/static/downloadaccelerator/download.js
+++ downloadacceleratorsite/downloadaccelerator/static/downloadaccelerator/download.js
@@ -2,51 +2,33 @@ $(function() {
     /**
      * management of password button
      */
-    $('#togglePassword').click(
-      () => {
-        var newType = $('#password').attr('type') === 'password' ? 'text' : 'password';
-        $('#password').attr('type', newType);
-        $('#togglePassword').toggleClass('fa-eye-slash').toggleClass('fa-eye');
+    $('.fa-eye-slash').click(
+      function () {
+        var newType = $(this).siblings('input').attr('type') === 'password' ? 'text' : 'password';
+        $(this).siblings('input').attr('type', newType);
+        $(this).toggleClass('fa-eye-slash').toggleClass('fa-eye');
       }
     );
+    
 
-    $('#togglePassword2').click(
-        () => {
-          var newType = $('#password2').attr('type') === 'password' ? 'text' : 'password';
-          $('#password2').attr('type', newType);
-          $('#togglePassword2').toggleClass('fa-eye-slash').toggleClass('fa-eye');
-        }
-    );
     /**
-     * management of name input text
+     * management of name submitting button
      */
     
     $('#btn-createAccount').click(
-      () => {
-        if (($('#name').val() === "")|(!(/^[a-zA-Z][a-zA-Z0-9_-]*$/.test($('#name').val())))){
+      function () {
+        if(($('#name').val().length === 0) | (!(/^[a-zA-Z][a-zA-Z0-9_-]*$/.test($('#name').val())))){
           $('#name').css('border-color', 'red');
-          $('#name').tooltip();
         }
         else {
           $('#name').css('border-color', 'blue');
         }
-        if(($('#email').val() === "")|(!(/[a-zA-Z][a-zA-Z0-9_-]*@[a-zA-Z]+\.[a-z]{2,5}/.test($('#email').val())))){
-          $('#email').css('border-color', 'red');
-          $('#email').tooltip();
-        }
-        if($('#password').val() === ""){
-          $('#password').css('border-color', 'red');
-          $('#password').tooltip();
-        }
-        if($('#password2').val() === ""){
-          $('#password2').css('border-color', 'red');
-          $('#password2').tooltip();
-        }
-        if($('#password').val() != $('#password2').val()){
-          $('#password').css('border-color', 'red');
-          $('#password2').css('border-color', 'red');
+        if($('input[placeholder = "Create password"]').val() != $('input[placeholder = "Repeat password"]').val()){
+          $('input[placeholder = "Create password"]').css('border-color', 'red');
+          $('input[placeholder = "Repeat password"]').css('border-color', 'red');
         }
       }
     );
+    
     
 });
