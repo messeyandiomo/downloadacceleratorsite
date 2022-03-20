c50a2ee315d618d776c23b6a92afcabb688cad7c
bilal messey <messey.bilal@gmail.com>
2022-03-20 13:32:29 +0100

    sixteenth update: updating of download.js page

--- downloadacceleratorsite/downloadaccelerator/urls.py
+++ downloadacceleratorsite/downloadaccelerator/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('home.html', views.home, name='home'),
     path('features.html', views.features, name='features'),
     path('download.html', views.download, name='download'),
-    path('<str:username>', views.checkUser, name='user'),
+    path('username', views.checkUser, name='user'),
 ]
--- downloadacceleratorsite/downloadaccelerator/views.py
+++ downloadacceleratorsite/downloadaccelerator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -11,5 +12,11 @@ def features(request):
 def download(request):
     return render(request, 'downloadaccelerator/download.html')
 
-def checkUser(request, username):
-    return User.objects.filter(username=username).exists()
+def checkUser(request):
+    if request.is_ajax and request.method == 'GET':
+        username = request.GET.get("username", None)
+        if User.objects.filter(username = username).exists():
+            return JsonResponse({"valid": False}, status=200)
+        else:
+            return JsonResponse({"valid": True}, status=300)
+    return JsonResponse({}, status=400)
