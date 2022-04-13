ab5e824a9db2c19bb2505fbb2ff971210ad0bd13
bilal messey <messey.bilal@gmail.com>
2022-04-13 17:54:22 +0100

    sixtieth update: updating of forums page by adding footer image

--- downloadacceleratorsite/downloadacceleratorsite/urls.py
+++ downloadacceleratorsite/downloadacceleratorsite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('downloadaccelerator.urls')),
-    #path('forums', include('Discussion_forum.urls')),
+    path('forums/', include('Discussion_forum.urls')),
 ]
